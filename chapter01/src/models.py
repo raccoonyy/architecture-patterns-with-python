5555ac4cc5249fa01479143bf37bc997c4f9a740
raccoony <raccoonyy@gmail.com>
2023-08-14 21:15:48 +0900

    Code for test_가용재고가_주문량보다_많으면_할당할_수_있다
    test_가용재고가_주문량보다_적으면_할당할_수_없다
    test_가용재고와_주문량이_같다면_할당할_수_있다

--- chapter01/src/models.py
+++ chapter01/src/models.py
@@ -19,6 +19,11 @@ class Batch:
     def allocate(self, line: OrderLine):
         self.lines.add(line)
 
+    def can_allocate(self, line: OrderLine):
+        if self.available_quantity < line.qty:
+            return False
+        return True
+
     @property
     def available_quantity(self):
         return self.qty - sum([line.qty for line in self.lines])
